c978956dbf57cd00a749aa2130f036b1ad2a20d8
Volkov <mark.volkovgit@gmail.com>
2021-11-25 08:33:48 +0200

    create migration power

---
+++ models/superpower.js
@@ -0,0 +1,37 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Superpower extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Superpower.belongsToMany(models.Superhero, {
+        through: 'heroes_to_superpowers',
+        foreignKey: 'powerId',
+      });
+    }
+  }
+  Superpower.init(
+    {
+      name: {
+        field: 'name',
+        allowNull: false,
+        type: DataTypes.STRING(64),
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Superpower',
+      tableName: 'superpowers',
+      underscored: true,
+    }
+  );
+  return Superpower;
+};
