ad86b4c2dc11468cbadaae7a4ae5549608186e90
Volkov <mark.volkovgit@gmail.com>
2021-11-25 09:13:26 +0200

    create model hero

---
+++ models/superhero.js
@@ -0,0 +1,60 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Superhero extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Superhero.belongsToMany(models.Superpower, {
+        through: 'heroes_to_superpowers',
+        foreignKey: 'heroId',
+      });
+    }
+  }
+  Superhero.init(
+    {
+      nickName: {
+        field: 'nick_name',
+        allowNull: false,
+        type: DataTypes.STRING(128),
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      realName: {
+        field: 'real_name',
+        allowNull: false,
+        type: DataTypes.STRING(128),
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      originDescription: {
+        field: 'origin_description',
+        type: DataTypes.STRING(512),
+        allowNull: true,
+      },
+      catchPhrase: {
+        field: 'catch_phrase',
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Superhero',
+      tableName: 'superheroes',
+      underscored: true,
+    }
+  );
+  return Superhero;
+};
