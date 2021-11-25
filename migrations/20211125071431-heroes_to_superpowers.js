244cc817b16a837721d9fe3c066691387c73bb44
Volkov <mark.volkovgit@gmail.com>
2021-11-25 09:17:00 +0200

    add model htp

---
+++ migrations/20211125071431-heroes_to_superpowers.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('heroes_to_superpowers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      heroId: {
+        field: 'hero_id',
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'superheroes',
+          key: 'id',
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      },
+      powerId: {
+        field: 'power_id',
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'superpowers',
+          key: 'id',
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('heroes_to_superpowers');
+  },
+};
